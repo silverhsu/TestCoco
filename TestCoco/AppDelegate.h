d2cb02f9ea2dc0c7f084886cb6a9220b6095638d
Yunke Liu <Yunke@null-002608e65aa3.wufi.wustl.edu>
2012-07-14 23:18:08 -0500

    Initial Commit

---
+++ TestCoco/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  TestCoco
+//
+//  Created by Yunke Liu on 7/14/12.
+//  Copyright Washington University in St. Louis 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
